f0d680a02b2ed5bf97b2fda981b6665d916b0820
“mamun8854” <mamunabdullah8854@gmail.com>
2025-06-29 09:43:22 +0600

    style: flake8

--- resumes/models/resume.py
+++ resumes/models/resume.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+
 class Resume(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     full_name = models.CharField(max_length=200)
