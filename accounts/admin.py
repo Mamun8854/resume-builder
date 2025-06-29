4495cd85b7eaaf7fd5102477ac8f31fd20922831
“mamun8854” <mamunabdullah8854@gmail.com>
2025-06-29 09:02:18 +0600

    feat: register user profile model in admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from accounts.models import Profile
 
-# Register your models here.
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'full_name', 'blood_group', 'profile_image')
+    search_fields = ('user__username', 'full_name', 'blood_group')
+    list_filter = ('blood_group',)
+    readonly_fields = ('user',)
+
+    def has_add_permission(self, request):
+        return False
+
+    def has_delete_permission(self, request, obj=None):
+        return False
