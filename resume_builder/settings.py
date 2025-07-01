08f7e982fbfe811fbab9c97b056667da2f451e97
“mamun8854” <mamunabdullah8854@gmail.com>
2025-07-02 00:30:26 +0600

    chore: static file location updated

--- resume_builder/settings.py
+++ resume_builder/settings.py
@@ -95,8 +95,6 @@ JAZZMIN_SETTINGS = {
     ],
 }
 
-
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -154,6 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
