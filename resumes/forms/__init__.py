a992e6fceb4d29acf650ead9b5ecc7f9c352db71
“mamun8854” <mamunabdullah8854@gmail.com>
2025-07-01 09:30:13 +0600

    chore: remove forms

--- resumes/forms/__init__.py
+++ resumes/forms/__init__.py
@@ -1,10 +1,5 @@
-from resumes.forms.experience import ExperienceForm, SkillForm
-from resumes.forms.education import EducationForm
 from resumes.forms.resume import ResumeForm
 
 __all__ = [
-    ResumeForm,
-    EducationForm,
-    ExperienceForm,
-    SkillForm,
+    ResumeForm
 ]
