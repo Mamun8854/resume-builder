801517a3c2f8a4a81b96413d1605db5375181294
“mamun8854” <mamunabdullah8854@gmail.com>
2025-07-01 23:22:58 +0600

    chore: remove file and add form to resume file

--- resumes/forms/education.py
+++
@@ -1,9 +0,0 @@
-from django.forms import inlineformset_factory
-from resumes.models import Education, Resume
-
-EducationFormSet = inlineformset_factory(
-    Resume, Education,
-    fields=['institution', 'degree', 'start_year', 'end_year'],
-    extra=1,
-    can_delete=True
-)
